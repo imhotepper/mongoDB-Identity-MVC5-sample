34d58020b61c4c29624dc15feb09f640ec4f4b4e
dragosh -  azure <stefanescu.dragos@gmail.com>
2014-05-16 19:55:39 +0300

    Owin Identity2 with mongodb backend Sample - init

---
+++ MongoDbIdentity/MongoDbCtx.cs
@@ -0,0 +1,65 @@
+﻿using AspNet.Identity.MongoDB;
+using MongoDB.Driver;
+using MongoDbIdentity.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace MongoDbIdentity
+{
+    public class MongoDbCtx
+    {
+        static ServerAndDb _serverAndDb;
+        static MongoClient Client ;
+        public  MongoDatabase Database  = Client.GetServer().GetDatabase(_serverAndDb.Database); 
+        public  MongoCollection<ApplicationUser> Users { get { return Database.GetCollection<ApplicationUser>("users"); } }
+
+        public  IdentityContext IdentityContext { get{return new IdentityContext(Users);}}
+
+         static MongoDbCtx()
+        {
+             _serverAndDb = new ServerAndDb(GetConnectionString());
+             Client  = new MongoClient(_serverAndDb.Server);
+             
+        }
+
+      #region Utils
+        private static string GetConnectionString()
+        {
+
+            return WebConfigurationManager.AppSettings.Get("MONGOHQ_URL") ??
+                  WebConfigurationManager.AppSettings.Get("MONGOLAB_URI") ??
+                  WebConfigurationManager.ConnectionStrings["MongoDBConnection"].ConnectionString;
+        }
+
+        #endregion
+
+
+
+    }
+
+
+    class ServerAndDb
+    {
+        public string Server { get; set; }
+        public string Database { get; set; }
+
+        public ServerAndDb(string fullConnectionString)
+        {
+            Trace.WriteLine("Preparing url parsing for: " + fullConnectionString);
+            if (!string.IsNullOrEmpty(fullConnectionString))
+            {
+                var slashIndex = fullConnectionString.LastIndexOf('/');
+                Server = fullConnectionString;
+                if (slashIndex < fullConnectionString.Length)
+                    Database = fullConnectionString.Substring(++slashIndex);
+
+                Trace.WriteLine("Parser done! Server: " + Server + ", Db: " + Database);
+            }
+
+        }
+    }
+}
---
+++ MongoDbIdentity/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MongoDbIdentity")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ING Group")]
+[assembly: AssemblyProduct("MongoDbIdentity")]
+[assembly: AssemblyCopyright("Copyright © ING Group 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4de0a327-4b20-4ef7-8ba2-91c3deb015b5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
